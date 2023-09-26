e1b0a5613b26c1163fb821f9f31e522e15106413
aivin1 <aivinni1219@gmail.com>
2023-09-26 10:24:11 -0400

    edits

--- src/Circle.java
+++ src/Circle.java
@@ -1,12 +1,18 @@
+import java.awt.*;
+
 public class Circle {
     private final double radius;
     private double area;
     public Circle(double r) {
         radius = r;
     }
-
-    public void findArea() {
+    public double calculateArea() {
         area = (radius * radius) * Math.PI;
+        return area;
+    }
+    public void findArea() {
+        Rectangle rect1 = new Rectangle();
+        area = rect1.getX() * rect1.getY();
         System.out.println(area);
     }
 }
